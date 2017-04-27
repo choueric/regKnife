531fd328f255eeeb4272cf624bf5016de7beffab
zhs <haishan.zhou@magewell.com>
2017-04-27 16:31:55 +0800

    add 'value' command to update value

--- main.go
+++ main.go
@@ -12,9 +12,10 @@ import (
 )
 
 var (
-	ui        *cli.ColoredUi
-	regLength = 32
-	binStr    string
+	ui     *cli.ColoredUi
+	regLen = 32
+	binStr string
+	value  int64
 )
 
 func initUi() error {
@@ -38,7 +39,7 @@ func initUi() error {
 	return nil
 }
 
-func genBinStr(val int64, regLen int) string {
+func genBinStr(val int64) string {
 	s := strconv.FormatInt(val, 2)
 	l := len(s)
 	bin := strings.Repeat("0", regLen-l)
@@ -64,7 +65,7 @@ func formateBinStr(bin string) string {
 func printAllFormat(bin string) {
 	dec, err := strconv.ParseInt(bin, 2, 64)
 	if err != nil {
-		fmt.Println("convert subbin to decimal failed:", err)
+		ui.Error(fmt.Sprintf("convert subbin to decimal failed: %v", err))
 		return
 	}
 
@@ -73,7 +74,7 @@ func printAllFormat(bin string) {
 	fmt.Printf("hex: 0x%x\n", dec)
 }
 
-func showBits(start, end int, val int64, regLen int) {
+func showBits(start, end int) {
 	start_index := regLen - 1 - end
 	end_index := regLen - 1 - start
 	subbin := binStr[start_index : end_index+1]
@@ -81,7 +82,7 @@ func showBits(start, end int, val int64, regLen int) {
 	printAllFormat(subbin)
 }
 
-func handleViewReg(val int64, input string, regLen int) {
+func handleViewReg(input string) {
 	var start, end int
 	if strings.Contains(input, ":") {
 		index := strings.Split(input, ":")
@@ -119,21 +120,39 @@ func handleViewReg(val int64, input string, regLen int) {
 		ui.Error(fmt.Sprintf("range is invalid, [%d, %d].", 0, regLen-1))
 		return
 	}
-	showBits(start, end, val, regLen)
+	showBits(start, end)
 }
 
-func handleInput(val int64, input string, regLen int) (exit bool) {
+func changeValue() string {
+	s, err := ui.Ask("input value:")
+	if err != nil {
+		ui.Error(fmt.Sprintf("get input failed: %v", err))
+		return ""
+	}
+
+	val, err := strconv.ParseInt(s, 0, 64)
+	if err != nil {
+		ui.Error(fmt.Sprintf("convert to Int failed: %v", err))
+		return ""
+	}
+	return genBinStr(val)
+}
+
+func handleInput(input string) (exit bool) {
 	exit = false
 	switch input {
 	case "exit":
 		exit = true
 		return
-	case "help":
+	case "help", "h":
 		printUsage()
-	case "print":
+	case "print", "p":
+		printAllFormat(binStr)
+	case "value", "v":
+		binStr = changeValue()
 		printAllFormat(binStr)
 	default:
-		handleViewReg(val, input, regLen)
+		handleViewReg(input)
 	}
 
 	return
@@ -141,29 +160,21 @@ func handleInput(val int64, input string, regLen int) (exit bool) {
 
 func printUsage() {
 	ui.Output("Usage:")
-	ui.Output("  help     : print this message.")
-	ui.Output("  exit     : exit this program.")
-	ui.Output("  print    : print input value.")
+	ui.Output("  [h]elp   : print this message.")
+	ui.Output("  [p]rint  : print input value.")
+	ui.Output("  [v]alue  : input value.")
 	ui.Output("  <patter> : bit number or range of bits, like 21, 12:14 or 14:12.")
+	ui.Output("  exit     : exit this program.")
 }
 
 func main() {
-	var valStr string
-	flag.IntVar(&regLength, "l", 32, "register length.")
-	flag.StringVar(&valStr, "v", "xx", "input value, 10 or 0x03.")
+	flag.IntVar(&regLen, "l", 32, "register length.")
 	flag.Parse()
 
-	if valStr == "xx" {
+	if len(os.Args) == 2 && (os.Args[1] == "help" || os.Args[1] == "-h") {
 		flag.Usage()
-		os.Exit(1)
+		os.Exit(0)
 	}
-	val, err := strconv.ParseInt(valStr, 0, 64)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(2)
-	}
-	binStr = genBinStr(val, regLength)
-	printAllFormat(binStr)
 
 	if err := initUi(); err != nil {
 		fmt.Println(err)
@@ -177,7 +188,7 @@ func main() {
 			fmt.Println(err)
 		}
 
-		if handleInput(val, input, regLength) {
+		if handleInput(input) {
 			break
 		}
 	}
